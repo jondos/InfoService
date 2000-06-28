79bc7d96e366e3cfafa488820eca8c63ba2a0b2f
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2000-06-28 13:23:21 +0000

    Some bugfixes...

--- InfoService/trunk/CAVersion.java
+++ InfoService/trunk/CAVersion.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;
 
 public class CAVersion
 	{
-		private static String aktVersion="00.00.007";
+		private static String aktVersion="00.00.008";
 		public static int checkForNewVersion(JAPModel model)
 			{
 				try
--- InfoService/trunk/JAP.java
+++ InfoService/trunk/JAP.java
@@ -1,19 +1,21 @@
 
-public class JAP {
+public final class JAP 
+	{
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv)
+			{
+				String vers = System.getProperty("java.version");
+				if (vers.compareTo("1.1.2") < 0)
+					{
+							System.out.println("!!!WARNING: JAP must be run with a " +
+			       "										1.1.2 or higher version VM!!!");
+					}
 
-	String vers = System.getProperty("java.version");
-	if (vers.compareTo("1.1.2") < 0) {
-	    System.out.println("!!!WARNING: JAP must be run with a " +
-			       "1.1.2 or higher version VM!!!");
-	}
-
-	JAPModel model = new JAPModel();
-	JAPSplash splash = new JAPSplash(model, model.TITLE);
-	splash.show(); // show splash screen as soon as possible
-	model.load();
-	model.addJAPObserver(model);
+				JAPModel model = new JAPModel();
+				JAPSplash splash = new JAPSplash(model);
+				splash.show(); // show splash screen as soon as possible
+				model.load();
+				model.addJAPObserver(model);
 
 	//Command line testing
 	/*
