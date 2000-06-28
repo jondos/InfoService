0df2968e63c3105b959c7bde1c4b88e33e8215d9
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2000-06-28 13:15:37 +0000

    Added some files...

---
+++ JAPSplash.java
@@ -0,0 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Window;
+import java.awt.Frame;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public final class JAPSplash extends Window 
+	{
+    public  JAPSplash (JAPModel m, String s)
+			{
+				super(new Frame());
+				//super(s);
+				JLabel waitLabel = new JLabel(m.getString("loading"), JLabel.CENTER);
+				waitLabel.setBackground(Color.black);
+				waitLabel.setForeground(Color.white);
+
+				/*getContentPane().*/setBackground(Color.black);
+				/*getContentPane().*/add(new JLabel(m.loadImageIcon(m.SPLASHFN, true)), BorderLayout.NORTH);
+				/*getContentPane().*/add(new JLabel(m.loadImageIcon(m.BUSYFN, true)), BorderLayout.CENTER);
+				/*getContentPane().*/add(waitLabel, BorderLayout.SOUTH);
+	
+				pack();
+				//setResizable(false);
+				m.centerFrame(this);
+				setVisible(true);
+		}
+}
+
+
