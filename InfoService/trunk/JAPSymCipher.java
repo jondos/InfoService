6c7a92cf20c235dab95a163d1b932976d9e92d82
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2000-07-14 15:51:39 +0000

    Anpassungen für RSA-Key-Generierung

--- InfoService/trunk/JAPSymCipher.java
+++ InfoService/trunk/JAPSymCipher.java
@@ -1,21 +1,38 @@
 import org.logi.crypto.keys.BlowfishKey;
+//import javax.crypto.Cipher;
+//import cryptix.jce.provider.key.RawSecretKey;
 
 public final class JAPSymCipher
 	{
-			private BlowfishKey bfEnc,bfDec;
+		
+		//	private Cipher cipherEnc;
+		//	private Cipher cipherDec;
+			BlowfishKey bfEnc,bfDec;
 			public JAPSymCipher()
 				{
 					bfEnc=bfDec=null;
+			//	try{cipherEnc=Cipher.getInstance("Blowfish/ECB/None");
+			//			cipherDec=Cipher.getInstance("Blowfish/ECB/None");}
+			//	catch(Exception e)
+			//	{
+			//		e.printStackTrace();
+			//	}
 				}
 			
 			public int setEncryptionKey(byte[] key)
 				{
+			//		RawSecretKey k=new RawSecretKey("Blowfish",key);
+		//			try{cipherEnc.init(Cipher.ENCRYPT_MODE,k);}
+	//				catch(Exception e){e.printStackTrace();}
 					bfEnc=new BlowfishKey(key);
 					return 0;
 				}
 			
 			public int setDecryptionKey(byte[] key)
 				{
+		//			RawSecretKey k=new RawSecretKey("Blowfish",key);
+	//				try{cipherEnc.init(Cipher.ENCRYPT_MODE,k);}
+//					catch(Exception e){e.printStackTrace();}
 					bfDec=new BlowfishKey(key);
 					return 0;
 				}
@@ -24,6 +41,9 @@ public final class JAPSymCipher
 				{
 					for(int i=0;i<buff.length;i+=8)
 						bfEnc.encrypt(buff,i,buff,i);
+	//			try{cipherEnc.doFinal(buff);}
+		//			catch(Exception e){e.printStackTrace();}
+				
 					return 0;
 				}
 			
@@ -31,13 +51,19 @@ public final class JAPSymCipher
 				{
 					for(int i=0;i<len;i+=8)
 						bfEnc.encrypt(from,i+ifrom,to,i+ito);
-					return 0;
+			/*	try{cipherEnc.doFinal(from,ifrom,len,to,ito);}
+					catch(Exception e){
+						System.out.println("e1");
+						e.printStackTrace();}
+				*/	return 0;
 				}
 
 			public int decrypt(byte[] buff)
 				{
 					for(int i=0;i<buff.length;i+=8)
 						bfDec.decrypt(buff,i,buff,i);
+				//try{cipherDec.doFinal(buff);}
+					//catch(Exception e){e.printStackTrace();}
 					return 0;
 				}
 			
--- InfoService/trunk/src/JAP.java
+++ InfoService/trunk/src/JAP.java
@@ -1,3 +1,5 @@
+//import java.security.Security;
+//import cryptix.jce.provider.Cryptix;
 public final class JAP 
 	{
 
@@ -9,9 +11,11 @@ public final class JAP
 							System.out.println("!!!WARNING: JAP must be run with a " +
 			       "1.1.2 or higher version VM!!!");
 					}
+	
 				JAPModel model = new JAPModel();
 				JAPSplash splash = new JAPSplash(model);
 				splash.show(); // show splash screen as soon as possible
+	//			Security.addProvider(new Cryptix());
 				model.load();
 				model.addJAPObserver(model);
 
@@ -20,6 +24,8 @@ public final class JAP
 				model.addJAPObserver(view);
 				splash.dispose();
 				view.show();
+				model.keypool=new JAPKeyPool(20,16);
+				model.keypool.run();
 
 			//	model.setAnonMode(true);
     }
