489228adc7f154363b9b4ffda4492101af75fbe3
feder <feder@7f34aca7-edc8-46a5-9550-c19c22dae522>
2000-07-31 03:58:06 +0000

    *** empty log message ***

--- src/JAP.java
+++ src/JAP.java
@@ -1,32 +1,74 @@
+/** Project Web Mixes
+
+ *  Java Anon Proxy
+
+ *  Date: Sun, 20 July 2000
+
+ */
+
+
+
 //import java.security.Security;
+
 //import cryptix.jce.provider.Cryptix;
+
 public final class JAP 
+
 	{
 
+
+
     public static void main(String[] argv)
+
 			{
+
 				String vers = System.getProperty("java.version");
+
 				if (vers.compareTo("1.1.2") < 0)
+
 					{
+
 							System.out.println("!!!WARNING: JAP must be run with a " +
+
 			       "1.1.2 or higher version VM!!!");
+
 					}
+
 	
+
 				JAPModel model = new JAPModel();
+
 				JAPSplash splash = new JAPSplash(model);
+
 				splash.show(); // show splash screen as soon as possible
+
 	//			Security.addProvider(new Cryptix());
+
 				model.load();
+
 				model.addJAPObserver(model);
 
 
+
+
+
 				JAPView view = new JAPView (model.TITLE);
+
 				model.addJAPObserver(view);
+
 				splash.dispose();
+
 				view.show();
+
 				model.keypool=new JAPKeyPool(20,16);
+
 				model.keypool.run();
 
+
+
 			//	model.setAnonMode(true);
+
     }
+
 }
+
