d169293faf88e26bf9e0ad1c2bfc691d504dffad
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2000-12-19 14:37:22 +0000

    Started seperating GUI and AnonService

---
+++ InfoService/trunk/src/anon/JAPAnonChannel.java
@@ -0,0 +1,90 @@
+/*
+Copyright (c) 2000, The JAP-Team 
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+	- Redistributions of source code must retain the above copyright notice, 
+	  this list of conditions and the following disclaimer.
+
+	- Redistributions in binary form must reproduce the above copyright notice, 
+	  this list of conditions and the following disclaimer in the documentation and/or 
+		other materials provided with the distribution.
+
+	- Neither the name of the University of Technology Dresden, Germany nor the names of its contributors 
+	  may be used to endorse or promote products derived from this software without specific 
+		prior written permission. 
+
+	
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS 
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+*/
+import java.io.InputStream;
+
+public final class JAPProxyConnection extends Thread 
+	{ 
+		private final JAPSocket inSocket; 
+		private final JAPMuxSocket outSocket;
+		private final int channel;
+
+    public JAPProxyConnection (JAPSocket s, int channelID, JAPMuxSocket muxSocket) 
+			{ 
+				inSocket = s;
+				channel=channelID; 
+				outSocket=muxSocket; 
+			}
+	
+		public void run()
+			{ 
+				InputStream fromClient=null;
+				try
+					{ 
+						fromClient= inSocket.getInputStream(); 
+						byte[] buff=new byte[JAPMuxSocket.DATA_SIZE]; 
+						int len=fromClient.read(buff,0,JAPMuxSocket.DATA_SIZE-3-outSocket.getChainLen()*JAPMuxSocket.KEY_SIZE);
+						if(len!=-1)
+							{
+								outSocket.send(channel,buff,(short)len);		
+								while((len=fromClient.read(buff,0,JAPMuxSocket.DATA_SIZE-3))!=-1)
+									{
+										if(len>0)
+											{
+												int ret=outSocket.send(channel,buff,(short)len);
+												if(ret==-1)
+													break;
+												if(ret==JAPMuxSocket.E_CHANNEL_SUSPENDED)
+													sleep(1000);
+											}
+									} 
+							}	
+					} // if (protocol....)  
+				catch(Exception e)
+					{
+					}
+				try
+					{ 
+						fromClient.close(); 
+					}
+				catch(Exception e)
+					{
+					} 
+				try
+					{
+						if(!inSocket.isClosed())
+							{ 
+								outSocket.close(channel);
+								inSocket.close(); 
+							} 
+					}
+				catch (Exception e)
+					{
+						JAPDebug.out(JAPDebug.EXCEPTION,JAPDebug.THREAD,"ProxyConnection - Exception while closing: "+e); 
+					}	 
+			} 
+	}
