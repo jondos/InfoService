7b33dcfe1ed748a1fcd62b9c481337fa9c8c4929
feder <feder@7f34aca7-edc8-46a5-9550-c19c22dae522>
2001-04-07 13:34:07 +0000

    no message

--- InfoService/trunk/JAPModel.java
+++ InfoService/trunk/JAPModel.java
@@ -58,7 +58,7 @@ import anon.JAPAnonServiceListener;
 /* This is the Model of All. It's a Singelton!*/
 public final class JAPModel implements JAPAnonServiceListener{
 
-	public static final String aktVersion = "00.01.008"; // Version of JAP
+	public static final String aktVersion = "00.01.009"; // Version of JAP
 
 	public  Vector            anonServerDatabase = null; // vector of all available mix cascades
 	private AnonServerDBEntry currentAnonService = null; // current anon service data object
