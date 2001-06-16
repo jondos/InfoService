b26f23c771972fde559137ff06b73a1de70ae020
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2001-06-16 08:22:09 +0000

    no message

--- JAPUtil.java
+++ JAPUtil.java
@@ -1,28 +1,28 @@
 /*
-Copyright (c) 2000, The JAP-Team 
+Copyright (c) 2000, The JAP-Team
 All rights reserved.
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-	- Redistributions of source code must retain the above copyright notice, 
+	- Redistributions of source code must retain the above copyright notice,
 	  this list of conditions and the following disclaimer.
 
-	- Redistributions in binary form must reproduce the above copyright notice, 
-	  this list of conditions and the following disclaimer in the documentation and/or 
+	- Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation and/or
 		other materials provided with the distribution.
 
-	- Neither the name of the University of Technology Dresden, Germany nor the names of its contributors 
-	  may be used to endorse or promote products derived from this software without specific 
-		prior written permission. 
+	- Neither the name of the University of Technology Dresden, Germany nor the names of its contributors
+	  may be used to endorse or promote products derived from this software without specific
+		prior written permission.
 
-	
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS 
-OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
 AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
 BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 */
 import java.util.zip.ZipEntry;
@@ -58,7 +58,7 @@ final class JAPUtil
 			{
 				try
 					{
-						ZipFile zold=null;	
+						ZipFile zold=null;
 						ZipInputStream zdiff=null;
 						ZipOutputStream znew=null;
 						ZipEntry ze=null;
@@ -72,7 +72,7 @@ final class JAPUtil
 								oldnames.put(ze.getName(),ze.getName());
 							}
 						zdiff=new ZipInputStream(new FileInputStream(diffJAR));
-						znew=new ZipOutputStream(new FileOutputStream(newJAR));		
+						znew=new ZipOutputStream(new FileOutputStream(newJAR));
 						znew.setLevel(9);
 						byte[] b=new byte[5000];
 						while((ze=zdiff.getNextEntry())!=null)
@@ -111,7 +111,7 @@ final class JAPUtil
 												else if(s.equalsIgnoreCase("move"))
 													System.out.println("move "+st.nextToken());
 												else
-													System.out.println("unkown: "+s);										
+													System.out.println("unkown: "+s);
 											}
 									}
 								zdiff.closeEntry();
@@ -147,9 +147,9 @@ final class JAPUtil
 								}
 								in.close();
 								znew.closeEntry();
-								
+
 							}
-						
+
 						znew.finish();
 						znew.flush();
 						znew.close();
@@ -163,23 +163,23 @@ final class JAPUtil
 				}
 			return 0;
 		}
-		
-		
-		
-		
+
+
+
+
 	public static boolean isPort(int port)
 		{
 			if((port<1)||(port>65536))
 				return false;
 			return true;
 		}
-	
+
 
 	public static int parseNodeInt(Element parent,String name,int defaultValue)
 		{
 			int i=defaultValue;
 			if(parent!=null)
-				try	
+				try
 					{
 						NodeList nl=parent.getElementsByTagName(name);
 						i=Integer.parseInt(nl.item(0).getFirstChild().getNodeValue());
@@ -194,7 +194,7 @@ final class JAPUtil
 		{
 			int i=defaultValue;
 			if(e!=null)
-				try	
+				try
 					{
 						Attr at=e.getAttributeNode(attr);
 						i=Integer.parseInt(at.getValue());
@@ -209,7 +209,7 @@ final class JAPUtil
 		{
 			boolean b=defaultValue;
 			if(n!=null)
-				try	
+				try
 					{
 						String tmpStr=n.getNodeValue();
 						if(tmpStr.equalsIgnoreCase("true"))
@@ -227,7 +227,7 @@ final class JAPUtil
 		{
 			String s=defaultValue;
 			if(n!=null)
-				try	
+				try
 					{
 						s=n.getNodeValue();
 					}
@@ -236,7 +236,7 @@ final class JAPUtil
 					}
 			return s;
 		}
-	
+
 	/** Writes a XML-Document to an Output-Stream. Since writing was not standardzieds
 	 * since JAXP 1.1 different Methods are tried
 	 */
@@ -249,14 +249,15 @@ final class JAPUtil
 							((com.sun.xml.tree.XmlDocument)doc).write(out);
 						}
 					catch(Throwable t1)
-						{ 
+						{
 							try
 								{ //For JAXP 1.1 (for Instance Apache Crimson/Xalan shipped with Java 1.4)
-									javax.xml.transform.Transformer t=
+									Object t=
 											javax.xml.transform.TransformerFactory.newInstance().newTransformer();
 									javax.xml.transform.Result r=new javax.xml.transform.stream.StreamResult(out);
 									javax.xml.transform.Source s=new javax.xml.transform.dom.DOMSource(doc);
-									t.transform(s,r);
+									javax.xml.transform.Transformer g=(javax.xml.transform.Transformer)t;
+//									g.transform(s,r);
 								}
 							catch(Throwable t2)
 								{
@@ -270,19 +271,19 @@ final class JAPUtil
 				}
 			return true;
 		}
-	
-	
+
+
 	/** Loads an Image from a File or a Resource.
 	 *	@param strImage the Resource or filename of the Image
 	 *	@param sync true if the loading is synchron, false if it should be asynchron
 	 */
-	public static ImageIcon loadImageIcon(String strImage, boolean sync) 
+	public static ImageIcon loadImageIcon(String strImage, boolean sync)
 		{
 			//JAPDebug.out(JAPDebug.DEBUG,JAPDebug.GUI,"JAPModel:Image "+strImage+" loading...");
 			boolean finished = false;
 			ImageIcon img = null;
 			// this is necessary to make shure that the images are loaded when contained in a JAP.jar
-			try 
+			try
 				{
 					img = new ImageIcon(JAPUtil.class.getResource(strImage));
 				}
@@ -315,7 +316,7 @@ final class JAPUtil
 				}
 		return img;
 	}
-	
+
 	public static void centerFrame(Window f) {
 		Dimension screenSize = f.getToolkit().getScreenSize();
 		try //JAVA 1.1
@@ -330,14 +331,14 @@ final class JAPUtil
 			}
 	}
 
-		public static void upRightFrame(Window f) 
+		public static void upRightFrame(Window f)
 			{
 				Dimension screenSize = f.getToolkit().getScreenSize();
 				Dimension ownSize = f.getSize();
 				f.setLocation((screenSize.width-ownSize.width ) , 0 );
 			}
-	
-    public static void setPerfectTableSize(JTable table,Dimension maxDimension) 
+
+    public static void setPerfectTableSize(JTable table,Dimension maxDimension)
 			{
 				TableModel tableModel = table.getModel();
 				int perfectWidth  = 0;
@@ -356,16 +357,16 @@ final class JAPUtil
 								Component component = headerRenderer.getTableCellRendererComponent(null,column.getHeaderValue(),false,false,0,0);
 								headerWidth     = component.getPreferredSize().width;
 								columnHeight    = component.getPreferredSize().height;
-							}	
+							}
 						if (tableModel.getRowCount() > 0)
-							{			
+							{
 								// look at every entry
 								TableCellRenderer tableCellRenderer = table.getDefaultRenderer(tableModel.getColumnClass(i));
 								int cellWidth = 0;
-								for (int row = 0; row < tableModel.getRowCount(); row++) 
+								for (int row = 0; row < tableModel.getRowCount(); row++)
 									{
 										Object object        = tableModel.getValueAt(row,i);
-										Component component						 = tableCellRenderer.getTableCellRendererComponent(table,object,false,false,row,i);	
+										Component component						 = tableCellRenderer.getTableCellRendererComponent(table,object,false,false,row,i);
 										cellWidth            = Math.max(cellWidth,component.getPreferredSize().width);
 										columnHeight        += component.getPreferredSize().height;
 									}
@@ -380,5 +381,5 @@ final class JAPUtil
 			perfectWidth  = Math.min(maxDimension.width ,perfectWidth + 30);
 			perfectHeight = Math.min(maxDimension.height,minimunColunmHeight);
 			table.setPreferredScrollableViewportSize(new Dimension(perfectWidth, perfectHeight));
-    }    
+    }
 }
