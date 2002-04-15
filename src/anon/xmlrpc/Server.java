88a2e8219888ceb37f33659087ab7c8ac1833746
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2002-04-15 16:21:36 +0000

    no message

--- src/anon/xmlrpc/Server.java
+++ src/anon/xmlrpc/Server.java
@@ -4,8 +4,16 @@ public abstract class Server
   {
     public abstract int start();
     public abstract void stop();
-    public static Server generateServer()
+    static public Server generateServer()
       {
-        return new ServerImpl();
+        try
+          {
+            Class c=Class.forName("anon.xmlrpc.ServerImpl");
+            return (Server)c.newInstance();
+          }
+        catch(Exception e)
+          {
+            return null;
+          }
       }
-  }
+ }
