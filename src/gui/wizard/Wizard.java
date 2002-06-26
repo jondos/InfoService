8a4ff012754a909a13c568cff2725f03bceec9e0
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2002-06-26 10:57:54 +0000

    no message

--- src/gui/wizard/Wizard.java
+++ src/gui/wizard/Wizard.java
@@ -28,7 +28,6 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMA
 
 package gui.wizard;
 
-import java.awt.Dialog;
 import java.util.Vector;
 
 public interface Wizard
