d1f90520d646185cbc2ffbb486a0b3e5fd1c412e
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2002-07-31 10:08:27 +0000

    Adde new class ProxyConnection for better Proxy (Firewall) Support

--- anon/AnonService.java
+++ anon/AnonService.java
@@ -37,7 +37,7 @@ import logging.Log;
 public interface AnonService
   {
     ///The version of the AnonLib
-    public final static String ANONLIB_VERSION="00.00.003";
+    public final static String ANONLIB_VERSION="00.00.004";
     /** Estabishes a connection to an AnonServer, which is described through the
      *  anonService parameter. This method must be called before any Channels could be created.
      *  @param anonService AnonServer to use
