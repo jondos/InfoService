3fe73da9114ec1ebe331e41531fb62bc55323fbd
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2003-04-30 14:16:59 +0000

    Some more BugFixes in Update process

--- InfoService/trunk/update/JAPFinishWizardPage.java
+++ InfoService/trunk/update/JAPFinishWizardPage.java
@@ -43,47 +43,34 @@ import javax.swing.border.EmptyBorder;
 public class JAPFinishWizardPage extends BasicWizardPage
 {
 
-private JLabel labelFinish;
-public JTextField tf_BackupOfJapJar;
-private GridBagLayout gridBagFinish;
-private GridBagConstraints constraintsFinish;
+public JLabel m_labelBackupOfJapJar;
 
   public JAPFinishWizardPage(String name)
   {
-
     setIcon(JAPUtil.loadImageIcon(JAPConstants.DOWNLOADFN,false));
     setPageTitle("Update-Wizard beenden");
-    gridBagFinish = new GridBagLayout();
-    constraintsFinish = new GridBagConstraints();
+    GridBagLayout gridBagFinish=new GridBagLayout();
+    GridBagConstraints constraintsFinish=new GridBagConstraints();
 
     m_panelComponents.setLayout(gridBagFinish);
-    labelFinish = new JLabel(JAPMessages.getString("updateFinishMessage"));
+    JLabel labelFinish = new JLabel(JAPMessages.getString("updateFinishMessage"));
     constraintsFinish.gridx = 0;
     constraintsFinish.gridy = 0;
-    constraintsFinish.gridwidth = 2;
-    constraintsFinish.anchor = GridBagConstraints.CENTER;
+    constraintsFinish.weightx=1;
+    constraintsFinish.fill=GridBagConstraints.HORIZONTAL;
+    constraintsFinish.anchor = GridBagConstraints.NORTHWEST;
     gridBagFinish.setConstraints(labelFinish,constraintsFinish);
     m_panelComponents.add(labelFinish, constraintsFinish);
 
-    tf_BackupOfJapJar = new JTextField();
-    tf_BackupOfJapJar.setEditable(false);
-    tf_BackupOfJapJar.setBorder(new EmptyBorder(new Insets(3,0,0,0)));
+    m_labelBackupOfJapJar = new JLabel();
     constraintsFinish.gridx = 0;
     constraintsFinish.gridy = 1;
     constraintsFinish.gridwidth = 2;
     constraintsFinish.anchor = GridBagConstraints.WEST;
     constraintsFinish.fill = GridBagConstraints.HORIZONTAL;
-    gridBagFinish.setConstraints(tf_BackupOfJapJar,constraintsFinish);
-    m_panelComponents.add(tf_BackupOfJapJar, constraintsFinish);
-
-    this.setVisible(true);
-    //createGridBagLayout();
-  // this.setIcon(this.getIcon());
-   //this.setComponentPanel();
-   //this.setPageTitle(name);
-   //this.createBorder(name);
-
-
+    gridBagFinish.setConstraints(m_labelBackupOfJapJar,constraintsFinish);
+    m_panelComponents.add(m_labelBackupOfJapJar);
+//    setVisible(true);
   }
 
 
