7816c7ef3bf5642d82f077a8446397883dbba59b
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2003-05-02 14:33:56 +0000

    no message

--- src/anon/xmlrpc/client/AnonServiceImplProxy.java
+++ src/anon/xmlrpc/client/AnonServiceImplProxy.java
@@ -13,87 +13,92 @@ import anon.AnonServiceEventListener;
 import logging.Log;
 
 public class AnonServiceImplProxy implements AnonService
-  {
-    InetAddress m_RpcServerHost;
-    int m_RpcServerPort;
+	{
+		InetAddress m_RpcServerHost;
+		int m_RpcServerPort;
+		int m_ClientID;
 
-    public AnonServiceImplProxy(InetAddress addr,int port)
-      {
-        m_RpcServerHost=addr;
-        m_RpcServerPort=port;
-      }
+		public AnonServiceImplProxy(InetAddress addr,int port) throws Exception
+			{
+				m_RpcServerHost=addr;
+				m_RpcServerPort=port;
+				Object o=doRemote("registerClient",new Vector());
+				m_ClientID=((Integer)o).intValue();
+			}
 
-   public int connect(AnonServer anonService)
-    {
-      return 0;
-    }
+		public int connect(AnonServer anonService)
+			{
+				return 0;
+			}
 
-    public void disconnect()
-      {
-      }
+		public void disconnect()
+			{
+			}
 
-    public AnonChannel createChannel(int type) throws ConnectException
-      {
-        try
-          {
-            Object o=doRemote("createChannel",new Vector());
-            return new ChannelProxy(1,this);
-          }
-        catch(Exception e)
-          {
-            throw new ConnectException("Could not connect");
-          }
-      }
+		public AnonChannel createChannel(int type) throws ConnectException
+			{
+				try
+					{
+						Vector v=new Vector();
+						v.addElement(new Integer(m_ClientID));
+						Object o=doRemote("createChannel",v);
+						return new ChannelProxy(((Integer)o).intValue(),this);
+					}
+				catch(Exception e)
+					{
+						throw new ConnectException("Could not connect");
+					}
+			}
 
-    public AnonChannel createChannel(InetAddress host,int port) throws ConnectException
-      {
-        return null;
-      }
+		public AnonChannel createChannel(InetAddress host,int port) throws ConnectException
+			{
+				return null;
+			}
 
-    public void addEventListener(AnonServiceEventListener l)
-      {
-      }
+		public void addEventListener(AnonServiceEventListener l)
+			{
+			}
 
-    public void removeEventListener(AnonServiceEventListener l)
-      {
-      }
+		public void removeEventListener(AnonServiceEventListener l)
+			{
+			}
 
-    public void setLogging(Log log)
-      {
-      }
+		public void setLogging(Log log)
+			{
+			}
 
 //Implementation
-    private Object doRemote(String method,Vector params)throws IOException
-      {
-        try
-          {
-            XmlRpcClientLite xmlrpc = new XmlRpcClientLite("http://localhost:8889/RPC2");//m_RpcServerHost.getHostAddress(),m_RpcServerPort);
-            return xmlrpc.execute ("ANONXMLRPC."+method, params);
-          }
-        catch(Exception e)
-          {
-            e.printStackTrace();
-            throw new IOException ("Error processing XML-RCP: "+method);
-          }
-      }
+		private Object doRemote(String method,Vector params)throws IOException
+			{
+				try
+					{
+						XmlRpcClientLite xmlrpc = new XmlRpcClientLite("http://localhost:8889/RPC2");//m_RpcServerHost.getHostAddress(),m_RpcServerPort);
+						return xmlrpc.execute ("ANONXMLRPC."+method, params);
+					}
+				catch(Exception e)
+					{
+						e.printStackTrace();
+						throw new IOException ("Error processing XML-RCP: "+method);
+					}
+			}
 
-    protected void send(int id,byte[]buff,int len) throws IOException
-      {
-        Object o=doRemote("channelOutputStreamWrite",new Vector());
-      }
+		protected void send(int id,byte[]buff,int len) throws IOException
+			{
+				Object o=doRemote("channelOutputStreamWrite",new Vector());
+			}
 
-    protected int recv(int id,byte[]buff,int len) throws IOException
-      {
-        Vector v=new Vector();
-        v.addElement(new Integer(id));
-        v.addElement(new Integer(len));
-        Object o=doRemote("channelInputStreamRead",v);
-        if(o instanceof byte[])
-          {
-            len=((byte[])o).length;
-            System.arraycopy(o,0,buff,0,len);
-            return len;
-          }
-        return 0;
-      }
-  }
+		protected int recv(int id,byte[]buff,int len) throws IOException
+			{
+				Vector v=new Vector();
+				v.addElement(new Integer(id));
+				v.addElement(new Integer(len));
+				Object o=doRemote("channelInputStreamRead",v);
+				if(o instanceof byte[])
+					{
+						len=((byte[])o).length;
+						System.arraycopy(o,0,buff,0,len);
+						return len;
+					}
+				return 0;
+			}
+	}
