cbf68c2d6845a66cf327ae3a88fa5e18941927e7
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2004-03-09 11:35:31 +0000

    Better InfoService UI and CertCheck

--- InfoService/trunk/src/jap/JAPiPAQ.java
+++ InfoService/trunk/src/jap/JAPiPAQ.java
@@ -1,46 +1,48 @@
 /*
-Copyright (c) 2000, The JAP-Team
-All rights reserved.
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
+ Copyright (c) 2000, The JAP-Team
+ All rights reserved.
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
 
-	- Redistributions of source code must retain the above copyright notice,
-		this list of conditions and the following disclaimer.
+ - Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
 
-	- Redistributions in binary form must reproduce the above copyright notice,
-		this list of conditions and the following disclaimer in the documentation and/or
-		other materials provided with the distribution.
+ - Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
 
-	- Neither the name of the University of Technology Dresden, Germany nor the names of its contributors
-		may be used to endorse or promote products derived from this software without specific
-		prior written permission.
+ - Neither the name of the University of Technology Dresden, Germany nor the names of its contributors
+  may be used to endorse or promote products derived from this software without specific
+  prior written permission.
 
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS
-OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-*/
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS
+ OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
+ BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
 package jap;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Insets;
-import java.util.Enumeration;
+
 import java.util.Locale;
-import logging.*;
+import javax.swing.JPanel;
+import logging.LogHolder;
+import logging.LogLevel;
+import logging.LogType;
 
-final public class JAPiPAQ {
+final public class JAPiPAQ
+{
 
-	static JAPView view=null;
+	static JAPView view = null;
 	public JAPiPAQ()
-		{
-		}
+	{
+	}
 
-public void startJAP(String strJapConfFile) {
+	public void startJAP(String strJapConfFile)
+	{
 		JAPModel.create().setSmallDisplay(true);
 		// Init Messages....
 		JAPMessages.init();
@@ -49,7 +51,7 @@ public void startJAP(String strJapConfFile) {
 		JAPController controller = JAPController.create();
 		// Create debugger object
 		LogHolder.setLogInstance(JAPDebug.getInstance());
-		JAPDebug.getInstance().setLogType(LogType.NET+LogType.GUI+LogType.THREAD+LogType.MISC);
+		JAPDebug.getInstance().setLogType(LogType.NET + LogType.GUI + LogType.THREAD + LogType.MISC);
 		JAPDebug.getInstance().setLogLevel(LogLevel.WARNING);
 		// load settings from config file
 		controller.loadConfigFile(strJapConfFile);
@@ -71,24 +73,25 @@ public void startJAP(String strJapConfFile) {
 	}
 
 	public JPanel getMainPanel()
-		{
-			return JAPController.getView().getMainPanel();
-		}
+	{
+		return JAPController.getView().getMainPanel();
+	}
 
-	public void  setLocale(Locale l)
-		{
-			JAPController.getController().setLocale(l);
-		}
+	public void setLocale(Locale l)
+	{
+		JAPController.getController().setLocale(l);
+	}
 
-	public static void main(String[] argv) {
+	public static void main(String[] argv)
+	{
 		JAPiPAQ japOniPAQ = new JAPiPAQ();
 		japOniPAQ.startJAP(null);
 		//Test
 		//JFrame frame=new JFrame("JAP");
 		//frame.setIconImage(JAPUtil.);
 		//frame.setContentPane(japOniPAQ.getMainPanel());
-		view.setSize(240,300);
-		view.setLocation(0,0);
+		view.setSize(240, 300);
+		view.setLocation(0, 0);
 		view.setResizable(false);
 		view.show();
 	}
