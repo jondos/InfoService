995140a50a35d7792c70f652a667cb9718d41fab
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2005-03-21 12:00:46 +0000

    New Help

--- src/gui/JAPAboutAutoScroller.java
+++ src/gui/JAPAboutAutoScroller.java
@@ -94,10 +94,10 @@ public final class JAPAboutAutoScroller extends Canvas implements Runnable
 		m_textArea = new JEditorPane();
 		m_textArea.setEditable(false);
 //		m_textArea.setHighlighter(null);
-		m_textArea.setEnabled(false);
+		//m_textArea.setEnabled(false);
 		m_textArea.setDoubleBuffered(false);
 		m_textArea.setBackground(new Color(204, 204, 204));
-		m_textArea.setSize(m_iScrollAreaWidth, 10000);
+	m_textArea.setSize(m_iScrollAreaWidth, 10000);
 		m_textArea.setContentType("text/html");
 		m_textArea.setText(htmlText.trim());
 		m_iTextHeight = m_textArea.getPreferredSize().height;
