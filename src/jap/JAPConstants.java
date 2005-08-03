c8c72e36db6ba66ec871136a38e0906b16a040b5
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2005-08-03 13:50:18 +0000

    no message

--- src/jap/JAPConstants.java
+++ src/jap/JAPConstants.java
@@ -35,15 +35,15 @@ import anon.tor.Tor;
 
 public final class JAPConstants
 {
-	public static final String aktVersion = "00.05.018"; //Never change the layout of this line!
+	public static final String aktVersion = "00.05.019"; //Never change the layout of this line!
 	private static final String CVS_GENERATED_RELEASE_DATE = "$Date$";
 
 	//Warning: This is a little bit tricky,
 	//because CVS will expand the $Date$
 	//to the date of the last commmit of this file
 
-	public final static boolean m_bReleasedVersion = false; //Set to true if this is a stable (release) Version
-	private static final String RELEASE_DATE = "2005/07/11 15:07:00"; // Set only to a Value, if m_bReleaseVersion=true
+	public final static boolean m_bReleasedVersion = true; //Set to true if this is a stable (release) Version
+	private static final String RELEASE_DATE = "2005/07/11 15:19:00"; // Set only to a Value, if m_bReleaseVersion=true
 
 	public static final String strReleaseDate; //The Release date of this version
 
