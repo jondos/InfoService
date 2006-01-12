ef8bbf2d104fbb82a3d261c55c356037754ac366
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-01-12 10:03:41 +0000

    no message

--- InfoService/trunk/src/infoservice/InfoServiceConnectionReader.java
+++ InfoService/trunk/src/infoservice/InfoServiceConnectionReader.java
@@ -29,77 +29,86 @@ package infoservice;
 
 import java.io.InputStream;
 
-
 /**
  * This is the implementation for reading data from an InputStream with a limitof bytes maximally
  * read.
  */
-public class InfoServiceConnectionReader {
-  
-  /**
-   * This stores the InputStream where the data is read from.
-   */
-  private InputStream m_inputStream;
+public class InfoServiceConnectionReader
+{
 
-  /**
-   * This stores the currently number of bytes which can be read from the InputStream until the
-   * data limit is exhausted.
-   */
-  private int m_byteLimit;
-  
-  
-  /**
-   * Constructs a new InfoServiceConnectionReader with a read-limitation for an InputStream.
-   *
-   * @param a_inputStream The InputStream where the data shall be read from.
-   * @param a_byteLimit The maximum number of bytes allowed to read from the stream.
-   */
-  public InfoServiceConnectionReader(InputStream a_inputStream, int a_byteLimit) {
-    m_inputStream = a_inputStream;
-    m_byteLimit = a_byteLimit;
-  }
-  
-  
-  /**
-   * Reads one byte from the underlying InputStream. If the call is successful, the limit of bytes
-   * able to read from the stream is also decremented by 1. If the end of the stream is reached or
-   * there was an exception while reading from the stream, the byte limit is not decremented. In
-   * the case of a read exception, this exception is thrown. If the byte limit is exhausted, also
-   * an exception is thrown.
-   *
-   * @return The byte read from the stream or -1, if the end of the stream was reached.
-   */
-  public int read() throws Exception {
-    boolean limitReached = false;
-    synchronized (this) {
-      if (m_byteLimit < 1) {
-        limitReached = true;
-      }
-      else {
-        m_byteLimit--;
-      }
-    }
-    if (limitReached == true) {
-      throw (new Exception("Cannot read more bytes, message size limit reached."));
-    }
-    int returnByte = -1;
-    try {
-      returnByte = m_inputStream.read();
-    }
-    catch (Exception e) {
-      synchronized (this) {
-        /* nothing was read -> re-increase the counter */
-        m_byteLimit++;
-      }
-      throw (e);
-    }  
-    if (returnByte == -1) {
-      synchronized (this) {
-        /* nothing was read -> re-increase the counter */
-        m_byteLimit++;
-      }
-    }
-    return returnByte;   
-  }
+	/**
+	 * This stores the InputStream where the data is read from.
+	 */
+	private InputStream m_inputStream;
 
-}   
+	/**
+	 * This stores the currently number of bytes which can be read from the InputStream until the
+	 * data limit is exhausted.
+	 */
+	private int m_byteLimit;
+
+	/**
+	 * Constructs a new InfoServiceConnectionReader with a read-limitation for an InputStream.
+	 *
+	 * @param a_inputStream The InputStream where the data shall be read from.
+	 * @param a_byteLimit The maximum number of bytes allowed to read from the stream.
+	 */
+	public InfoServiceConnectionReader(InputStream a_inputStream, int a_byteLimit)
+	{
+		m_inputStream = a_inputStream;
+		m_byteLimit = a_byteLimit;
+	}
+
+	/**
+	 * Reads one byte from the underlying InputStream. If the call is successful, the limit of bytes
+	 * able to read from the stream is also decremented by 1. If the end of the stream is reached or
+	 * there was an exception while reading from the stream, the byte limit is not decremented. In
+	 * the case of a read exception, this exception is thrown. If the byte limit is exhausted, also
+	 * an exception is thrown.
+	 *
+	 * @return The byte read from the stream or -1, if the end of the stream was reached.
+	 */
+	public int read() throws Exception
+	{
+		boolean limitReached = false;
+		synchronized (this)
+		{
+			if (m_byteLimit < 1)
+			{
+				limitReached = true;
+			}
+			else
+			{
+				m_byteLimit--;
+			}
+		}
+		if (limitReached == true)
+		{
+			throw (new Exception("Cannot read more bytes, message size limit reached."));
+		}
+		int returnByte = -1;
+		try
+		{
+			returnByte = m_inputStream.read();
+		}
+		catch (Exception e)
+		{
+			synchronized (this)
+			{
+				/* nothing was read -> re-increase the counter */
+				m_byteLimit++;
+			}
+			throw (e);
+		}
+		if (returnByte == -1)
+		{
+			synchronized (this)
+			{
+				/* nothing was read -> re-increase the counter */
+				m_byteLimit++;
+			}
+		}
+		return returnByte;
+	}
+
+}
