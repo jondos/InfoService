6a558def7669f156ae9fa93ba0dae56ac468da7f
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-01-23 22:35:09 +0000

    no message

--- InfoService/trunk/src/anon/mixminion/EMail.java
+++ InfoService/trunk/src/anon/mixminion/EMail.java
@@ -6,7 +6,7 @@ public class EMail
 {
     private String[] m_receiver = null;
     private String m_payload = null;
-    
+
     /**
      * The Constructor of an eMail, which scould be send over the MixMinion-Net
      * @param receiver, a list of receivers of this eMail
@@ -18,7 +18,7 @@ public class EMail
         this.m_payload  = payload;
     }
 
-    
+
     /**
      * @return the Receivers of this eMail
      */
@@ -33,11 +33,11 @@ public class EMail
     public Vector getReceiverAsVektor()
     {
         Vector v = new Vector();
-        for (int i=0; i<m_receiver.length; i++) v.add(m_receiver[i]);
+        for (int i=0; i<m_receiver.length; i++) v.addElement(m_receiver[i]);
         return v;
     }
 
-    
+
     /**
      * @return the Payload of this eMail
      */
@@ -45,17 +45,17 @@ public class EMail
     {
         return m_payload;
     }
-    
-    
+
+
     public String toString()
     {
         String ret = "";
         for (int i=0; i<m_receiver.length; i++)
         {
-            ret = ret + "[" + m_receiver[i] + "]\n"; 
+            ret = ret + "[" + m_receiver[i] + "]\n";
         }
         ret = ret + m_payload;
-        
+
         return ret;
     }
 }
