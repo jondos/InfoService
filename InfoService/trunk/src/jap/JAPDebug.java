01ddf873268613cebbcf86ac0160db46498a435e
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-08-27 07:00:53 +0000

    <Kein Kommentar eingegeben>

--- InfoService/trunk/src/jap/JAPDebug.java
+++ InfoService/trunk/src/jap/JAPDebug.java
@@ -385,6 +385,24 @@ final public class JAPDebug extends WindowAdapter implements ActionListener, Log
 			}
 			catch (Exception e1)
 			{}
+			m_textareaConsole.append("TotalMemory: " +
+									 JAPUtil.formatBytesValue(Runtime.getRuntime().totalMemory()) + "\n");
+			try
+			{
+				Long result = (Long)
+					Runtime.class.getMethod("maxMemory", new Class[0]).invoke(
+									   Runtime.getRuntime(), new Object[0]);
+				m_textareaConsole.append("MaxMemory: " +
+										 JAPUtil.formatBytesValue(result.longValue()) + "\n");
+			}
+			catch (Exception a_e)
+			{
+				// ignore
+			}
+
+			m_textareaConsole.append("FreeMemory: " +
+									 JAPUtil.formatBytesValue(Runtime.getRuntime().freeMemory()) + "\n");
+
 			m_textareaConsole.append(JAPModel.getInstance().toString());
 		}
 		else
