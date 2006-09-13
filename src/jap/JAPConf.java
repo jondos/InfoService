ab5e0f5e5872cf1276704879d6a928f845d0465f
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-09-13 21:02:56 +0000

    <Kein Kommentar eingegeben>

--- src/jap/JAPConf.java
+++ src/jap/JAPConf.java
@@ -92,6 +92,8 @@ final public class JAPConf extends JAPDialog implements ActionListener, Observer
 	private static final String MSG_ASK_RESET_DEFAULTS = JAPConf.class.getName() + "_askResetDefaults";
 	private static final String MSG_NEED_RESTART = JAPConf.class.getName() + "_needRestart";
 	private static final String MSG_LISTENER_CHANGED = JAPConf.class.getName() + "_listenerChanged";
+	private static final String MSG_ACCESS_TO_JAP = JAPConf.class.getName() + "_accessToJAP";
+
 
 
 	final static public String PORT_TAB = "PORT_TAB";
@@ -1002,7 +1004,7 @@ final public class JAPConf extends JAPDialog implements ActionListener, Observer
 			{
 				public String getName()
 				{
-					return JAPMessages.getString("Port");
+					return JAPMessages.getString("Portlistener");
 				}
 
 				public String getMessage()
@@ -1022,7 +1024,7 @@ final public class JAPConf extends JAPDialog implements ActionListener, Observer
 			{
 				public String getName()
 				{
-					return JAPMessages.getString("Local proxy");
+					return JAPMessages.getString(JAPMessages.getString(MSG_ACCESS_TO_JAP));
 				}
 
 				public void doChange()
