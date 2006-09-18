0113145aa77030b06b75c87ef11149e510433844
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-09-18 15:41:33 +0000

    <Kein Kommentar eingegeben>

--- InfoService/trunk/src/jap/JAPController.java
+++ InfoService/trunk/src/jap/JAPController.java
@@ -2298,8 +2298,7 @@ public final class JAPController extends Observable implements IProxyListener, O
 				// this is needed to interrupt the connection process
 				if (m_proxyDirect == null)
 				{
-					msgIdConnect = m_View.addStatusMsg(JAPMessages.getString(
-						"setAnonModeSplashDisconnect"),
+					msgIdConnect = m_View.addStatusMsg(JAPMessages.getString(MSG_FINISHING_ANON),
 						JAPDialog.MESSAGE_TYPE_INFORMATION, false);
 				}
 				try
@@ -2662,8 +2661,7 @@ public final class JAPController extends Observable implements IProxyListener, O
 					AnonProxy proxyAnon = m_proxyAnon;
 					if (proxyAnon != null)
 					{
-						msgIdConnect = m_View.addStatusMsg(JAPMessages.getString(
-											  "setAnonModeSplashDisconnect"),
+						msgIdConnect = m_View.addStatusMsg(JAPMessages.getString(MSG_FINISHING_ANON),
 							JAPDialog.MESSAGE_TYPE_INFORMATION, false);
 						proxyAnon.stop();
 						m_View.removeStatusMsg(msgIdConnect);
