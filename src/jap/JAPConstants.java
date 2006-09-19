595db60bd4a22b6dfa9a2f30883436364fe56926
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-09-19 16:27:40 +0000

    <Kein Kommentar eingegeben>

--- src/jap/JAPConstants.java
+++ src/jap/JAPConstants.java
@@ -35,7 +35,7 @@ import anon.mixminion.Mixminion;
 
 public final class JAPConstants
 {
-	public static final String aktVersion = "00.05.418"; //Never change the layout of this line!
+	public static final String aktVersion = "00.05.419"; //Never change the layout of this line!
 	private static final String CVS_GENERATED_RELEASE_DATE = "$Date$";
 
 	//Warning: This is a little bit tricky,
