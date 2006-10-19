0411ee81a0ac69698ff2e03eb485e7e4a792bf2a
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-10-19 10:02:50 +0000

    no message

--- InfoService/trunk/src/infoservice/KeyGenTest.java
+++ InfoService/trunk/src/infoservice/KeyGenTest.java
@@ -16,25 +16,39 @@ import anon.crypto.X509SubjectKeyIdentifier;
 
 public class KeyGenTest
 {
-	private static String ms_strInfoServiceName="InfoService";
 
-
-	public static void generateKeys() throws IOException
+	public static void generateKeys(String isName, String passwd) throws IOException
 	{
 		PKCS12 ownCertificate;
-		String strPasswd="";
-		System.out.print("Please enter a name for the InfoService: ");
-		BufferedReader din=new BufferedReader(new InputStreamReader(System.in));
-		ms_strInfoServiceName=din.readLine();
-		System.out.print("Please enter a password to protect the private key of the InfoService: ");
-		strPasswd=din.readLine();
+		String strInfoServiceName;
+		if (isName != null && isName.length() > 0)
+		{
+			strInfoServiceName = isName;
+		}
+		else
+		{
+			System.out.print("Please enter a name for the InfoService: ");
+			BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
+			strInfoServiceName = din.readLine();
+		}
+		String strPasswd = "";
+		if (passwd == null)
+		{
+			System.out.print("Please enter a password to protect the private key of the InfoService: ");
+			BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
+			strPasswd = din.readLine();
+		}
+		else
+		{
+			strPasswd = passwd;
+		}
 		System.out.println("Key generation started!");
 		DSAKeyPair keyPair = DSAKeyPair.getInstance(new SecureRandom(), 1024, 80);
 		FileOutputStream out1 = new FileOutputStream("private.pfx");
 		FileOutputStream out2 = new FileOutputStream("public.cer");
 		X509Extensions extensions = new X509Extensions(new X509SubjectKeyIdentifier(keyPair.getPublic()));
-		ownCertificate = new PKCS12(new X509DistinguishedName("cn="+ms_strInfoServiceName), keyPair,
-									new Validity(new GregorianCalendar(),5), extensions);
+		ownCertificate = new PKCS12(new X509DistinguishedName("cn=" + strInfoServiceName), keyPair,
+									new Validity(new GregorianCalendar(), 5), extensions);
 		ownCertificate.store(out1, strPasswd.toCharArray());
 		ownCertificate.getX509Certificate().store(out2);
 		out1.close();
