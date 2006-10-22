b208f98fd6a0cacba75230342924defdcb266df5
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-10-22 13:21:18 +0000

    <Kein Kommentar eingegeben>

--- src/anon/infoservice/Constants.java
+++ src/anon/infoservice/Constants.java
@@ -97,6 +97,6 @@ final public class Constants
 	/**
 	 * This is the version number of the infoservice software.
 	 */
-	public static final String INFOSERVICE_VERSION = "IS.07.050"; //never change the layout of this line!
+	public static final String INFOSERVICE_VERSION = "IS.07.051"; //never change the layout of this line!
 
 }
