ae0064169d5b772975e286f9489b3a236f699121
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-10-23 20:36:57 +0000

    <Kein Kommentar eingegeben>

--- InfoService/trunk/src/infoservice/ISRuntimeStatistics.java
+++ InfoService/trunk/src/infoservice/ISRuntimeStatistics.java
@@ -13,6 +13,42 @@ final class ISRuntimeStatistics
 	///How many get /mixcascadestatus command did we process?
 	static volatile long ms_lNrOfGetMixCascadeStatusRequests=0;
 
+	///How many post commands did we process?
+	static volatile long ms_lNrOfPosts=0;
+
+	///How many post commands did we process?
+	static volatile long ms_lNrOfGets=0;
+
+	///How many unknown commands did we process?
+	static volatile long ms_lNrOfUnknownRequests=0;
+
+	///How many commands that were not get/post did we process?
+	static volatile long ms_lNrOfOtherMethod=0;
+
+	///How many get /mixinfo/ commands did we process?
+	static volatile long ms_lNrOfGetMixinfoRequests=0;
+
+	///How many get /cascadeinfo/ commands did we process?
+	static volatile long ms_lNrOfGetCascadeinfoRequests=0;
+
+	///How many get /infoservices commands did we process?
+	static volatile long ms_lNrOfGetInfoservicesRequests=0;
+
+	///How many get /infoserviceserials commands did we process?
+	static volatile long ms_lNrOfGetInfoserviceserialsRequests=0;
+
+	///How many get /cascadeserials commands did we process?
+	static volatile long ms_lNrOfGetCascadeserialsRequests=0;
+
+	///How many get /cascades commands did we process?
+	static volatile long ms_lNrOfGetCascadesRequests=0;
+
+	///How many get commands for forwarding did we process?
+	static volatile long ms_lNrOfGetForwarding=0;
+
+	///How many get /paymentinstances or /paymentinstance commands did we process?
+	static volatile long ms_lNrOfGetPaymentRequests=0;
+
 	///How many get /tornodes command did we process?
 	static volatile long ms_lNrOfGetTorNodesRequests=0;
 
@@ -27,12 +63,37 @@ final class ISRuntimeStatistics
 		sb.append("<table>");
 		sb.append("<tr><td>TCP/IP Connections received: </td><td>");
 		sb.append(ms_NumberFormat.format(ms_lTCPIPConnections));
+		sb.append("</td></tr><tr><td>Total GET Requests: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGets));
+		sb.append("</td></tr><tr><td>Total POST Requests: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfPosts));
+		sb.append("</td></tr><tr><td>Total other than GET/POST Requests: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfOtherMethod));
+		sb.append("</td></tr><tr><td><br></td><td>");
 		sb.append("</td></tr><tr><td>GET Requests for Cascade Status: </td><td>");
 		sb.append(ms_NumberFormat.format(ms_lNrOfGetMixCascadeStatusRequests));
-		sb.append("</td></tr><tr><td>GET Requests for Tor Nodes: </td><td>");
+		sb.append("</td></tr><tr><td>GET Requests for /cascadeserials: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetCascadeserialsRequests));
+		sb.append("</td></tr><tr><td>GET Requests for /cascades: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetCascadesRequests));
+		sb.append("</td></tr><tr><td>GET Requests for /infoserviceserials: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetInfoserviceserialsRequests));
+		sb.append("</td></tr><tr><td>GET Requests for /infoservices: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetInfoservicesRequests));
+		sb.append("</td></tr><tr><td>GET Requests for /mixinfo/: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetMixinfoRequests));
+		sb.append("</td></tr><tr><td>GET Requests for /cascadeinfo/: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetCascadeinfoRequests));
+		sb.append("</td></tr><tr><td>GET Requests for Forwarding: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetForwarding));
+		sb.append("</td></tr><tr><td>GET Requests for /tornodes: </td><td>");
 		sb.append(ms_NumberFormat.format(ms_lNrOfGetTorNodesRequests));
 		sb.append("</td></tr><tr><td>GET Requests for Min JAP Version: </td><td>");
 		sb.append(ms_NumberFormat.format(ms_lNrOfGetMinJapVersion));
+		sb.append("</td></tr><tr><td>GET Requests for Payment: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfGetPaymentRequests));
+		sb.append("</td></tr><tr><td>Unknown Requests: </td><td>");
+		sb.append(ms_NumberFormat.format(ms_lNrOfUnknownRequests));
 		sb.append("</td></tr><tr><td>Total Memory: </td><td>");
 		sb.append(ms_NumberFormat.format(Runtime.getRuntime().totalMemory()));
 		sb.append("</td></tr><tr><td>Free Memory: </td><td>");
