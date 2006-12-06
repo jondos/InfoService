44cf1f42808fd9cff2b9c6ef80ca4db06d818f57
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2006-12-06 11:57:25 +0000

    <Kein Kommentar eingegeben>

--- src/jap/SystrayPopupMenu.java
+++ src/jap/SystrayPopupMenu.java
@@ -31,6 +31,7 @@ import java.util.Vector;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Font;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -41,12 +42,17 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
+import anon.infoservice.Database;
+import anon.infoservice.StatusInfo;
 import anon.infoservice.MixCascade;
 import gui.JAPDll;
 import gui.JAPHelp;
 import gui.JAPMessages;
 import gui.PopupMenu;
 import gui.GUIUtils;
+import java.awt.GridBagConstraints;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
 
 /**
  *
@@ -58,6 +64,10 @@ public class SystrayPopupMenu extends PopupMenu
 	private static final String MSG_EXIT = SystrayPopupMenu.class.getName() + "_exit";
 	private static final String MSG_SHOW_MAIN_WINDOW = SystrayPopupMenu.class.getName() + "_showMainWindow";
 	private static final String MSG_ANONYMITY_MODE = SystrayPopupMenu.class.getName() + "_anonymityMode";
+	private static final String MSG_CURRENT_SERVICE = SystrayPopupMenu.class.getName() + "_currentService";
+	private static final String MSG_CONNECTED = SystrayPopupMenu.class.getName() + "_connected";
+	private static final String MSG_NOT_CONNECTED = SystrayPopupMenu.class.getName() + "_notConnected";
+	private static final String MSG_USER_NUMBER = SystrayPopupMenu.class.getName() + "_userNumber";
 
 	private MainWindowListener m_mainWindowListener;
 
@@ -73,6 +83,35 @@ public class SystrayPopupMenu extends PopupMenu
 		JLabel label;
 		ImageIcon icon;
 		MixCascade cascade = JAPController.getInstance().getCurrentMixCascade();
+		TrustModel currentModel = TrustModel.getCurrentTrustModel();
+		String connected;
+		String users = "";
+		if (JAPController.getInstance().isAnonConnected())
+		{
+			StatusInfo info =
+				(StatusInfo)Database.getInstance(StatusInfo.class).getEntryById(cascade.getId());
+			if (info != null)
+			{
+				users = ", " + JAPMessages.getString(MSG_USER_NUMBER, new Integer(info.getNrOfActiveUsers()));
+			}
+
+			connected = JAPMessages.getString(MSG_CONNECTED);
+		}
+		else
+		{
+			connected = JAPMessages.getString(MSG_NOT_CONNECTED);
+		}
+
+		label = new JLabel(JAPMessages.getString(MSG_CURRENT_SERVICE, new Object[]{connected, users}));
+		GUIUtils.setFontStyle(label, Font.PLAIN);
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.anchor = GridBagConstraints.CENTER;
+		panel.add(label, constraints);
+		getPopupMenu().add(panel);
+
 
 		if (cascade.isPayment())
 		{
@@ -87,11 +126,25 @@ public class SystrayPopupMenu extends PopupMenu
 			icon = GUIUtils.loadImageIcon(JAPConstants.IMAGE_CASCADE_INTERNET);
 		}
 		label = new JLabel(GUIUtils.trim(cascade.getName()));
+		GUIUtils.setFontStyle(label, Font.BOLD);
 		label.setIcon(icon);
-		getPopupMenu().add(label);
+		//label.setOpaque(true);
+		//label.setBackground(getPopupMenu().getBackground());
+		panel = new JPanel(new GridBagLayout());
+		//panel.setOpaque(true);
+		//panel.setBackground(getPopupMenu().getBackground());
+		constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.weightx = 1.0;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.anchor = GridBagConstraints.WEST;
+		panel.add(label, constraints);
+		getPopupMenu().add(panel);
 
 
 		final JCheckBoxMenuItem cbxMenuItem = new JCheckBoxMenuItem(JAPMessages.getString(MSG_ANONYMITY_MODE));
+		GUIUtils.setFontStyle(cbxMenuItem, Font.PLAIN);
 		cbxMenuItem.setSelected(JAPController.getInstance().getAnonMode());
 		cbxMenuItem.addChangeListener(new ChangeListener()
 		{
@@ -104,6 +157,7 @@ public class SystrayPopupMenu extends PopupMenu
 
 		getPopupMenu().addSeparator();
 		menuItem = new JMenuItem(JAPMessages.getString(MSG_SHOW_MAIN_WINDOW));
+		GUIUtils.setFontStyle(menuItem, Font.PLAIN);
 		menuItem.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent a_event)
@@ -114,13 +168,16 @@ public class SystrayPopupMenu extends PopupMenu
 		getPopupMenu().add(menuItem);
 
 		menuItem = new JMenuItem(JAPMessages.getString(JAPHelp.MSG_HELP_MENU_ITEM));
+		GUIUtils.setFontStyle(menuItem, Font.PLAIN);
 		getPopupMenu().add(menuItem);
 		menuItem.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent a_event)
 			{
 				JAPHelp.getInstance().getContextObj().setContext("index");
+				JAPHelp.getInstance().setAlwaysOnTop(true);
 				JAPHelp.getInstance().setVisible(true);
+				JAPHelp.getInstance().setAlwaysOnTop(false);
 			}
 		});
 
@@ -134,6 +191,15 @@ public class SystrayPopupMenu extends PopupMenu
 			if (tmpModel.isAdded())
 			{
 				filterMenu = new JMenu(tmpModel.getName());
+				if (currentModel.equals(tmpModel))
+				{
+					GUIUtils.setFontStyle(filterMenu, Font.BOLD);
+				}
+				else
+				{
+					GUIUtils.setFontStyle(filterMenu, Font.PLAIN);
+				}
+
 				final CascadePopupMenu tmpPopupCascade = new CascadePopupMenu(filterMenu.getPopupMenu());
 				getPopupMenu().add(filterMenu);
 
@@ -157,6 +223,7 @@ public class SystrayPopupMenu extends PopupMenu
 
 		getPopupMenu().addSeparator();
 		menuItem = new JMenuItem(JAPMessages.getString(MSG_EXIT));
+		GUIUtils.setFontStyle(menuItem, Font.PLAIN);
 		menuItem.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent a_event)
