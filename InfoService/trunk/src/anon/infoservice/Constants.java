21344266eefbc143d8ea4c8974560b289d4c5ca1
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2007-02-22 23:20:47 +0000

    no message

--- InfoService/trunk/src/anon/infoservice/Constants.java
+++ InfoService/trunk/src/anon/infoservice/Constants.java
@@ -98,7 +98,7 @@ final public class Constants
 	/**
 	 * This is the version number of the infoservice software.
 	 */
-	public static final String INFOSERVICE_VERSION = "IS.07.067"; //never change the layout of this line!
+	public static final String INFOSERVICE_VERSION = "IS.07.068"; //never change the layout of this line!
 
     public static final String DYNAMIC_CONFIGURATION_FILENAME = "dynamic.properties";
 }
