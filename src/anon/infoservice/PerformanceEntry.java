a82d82fbc8692ea3aa7d159ab376e5c7c0593e22
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2008-04-17 21:37:18 +0000

    - Synchronized with svn - some preparations for performance monitoring - new eclipse/jbuilder project files

--- src/anon/infoservice/PerformanceEntry.java
+++ src/anon/infoservice/PerformanceEntry.java
@@ -1,6 +1,13 @@
 package anon.infoservice;
 
-public class PerformanceEntry extends AbstractDatabaseEntry 
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
+
+import anon.util.XMLUtil;
+import anon.util.XMLParseException;
+import anon.util.IXMLEncodable;
+
+public class PerformanceEntry extends AbstractDatabaseEntry implements IXMLEncodable
 {
 	private String m_strCascadeId;
 	private String m_strInfoServiceId;
@@ -8,17 +15,23 @@ public class PerformanceEntry extends AbstractDatabaseEntry
 	private long m_lastUpdate;
 	private long m_serial;
 	
-	private static final int ENTRY_TIMEOUT = 1000*60*5;
-	
 	private long m_lDelay;
 	private double m_dSpeed;
 	
 	private long[] m_aDelays = new long[3];
 	private double[] m_aSpeeds = new double[3];
 	
-	public PerformanceEntry(String a_strCascadeId, String a_strInfoServiceId)
+	public static final String XML_ELEMENT_NAME = "PerformanceEntry";
+	public static final String XML_ELEMENT_CONTAINER_NAME = "PerformanceEntries";
+	
+	public static final String XML_ATTR_ID = "id";
+	public static final String XML_ATTR_AVG_DELAY = "avgDelay";
+	public static final String XML_ATTR_AVG_SPEED = "avgSpeed";
+	public static final String XML_ATTR_EXPIRE_TIME = "expireTime";
+	
+	public PerformanceEntry(String a_strCascadeId, String a_strInfoServiceId, long a_lExpireTime)
 	{
-		super(System.currentTimeMillis() + ENTRY_TIMEOUT);
+		super(a_lExpireTime);
 		
 		m_strCascadeId = a_strCascadeId;
 		m_strInfoServiceId = a_strInfoServiceId;
@@ -29,11 +42,35 @@ public class PerformanceEntry extends AbstractDatabaseEntry
 		m_dSpeed = -1;
 	}
 	
+	public PerformanceEntry(Element a_entry) throws XMLParseException
+	{
+		// TODO: client expire time
+		super(System.currentTimeMillis() + 1000*60*24);
+		
+		XMLUtil.assertNodeName(a_entry, XML_ELEMENT_NAME);
+		
+		String id = XMLUtil.parseAttribute(a_entry, XML_ATTR_ID, ".");
+		int i = id.indexOf(".");
+		
+		if(i < 0)
+		{
+			throw new XMLParseException(XML_ELEMENT_NAME + ": Could not parse id");
+		}
+		
+		m_strCascadeId = id.substring(0, i);
+		m_strInfoServiceId = id.substring(i);
+		m_lDelay = XMLUtil.parseAttribute(a_entry, XML_ATTR_AVG_DELAY, -1);
+		m_dSpeed = XMLUtil.parseAttribute(a_entry, XML_ATTR_AVG_SPEED, -1);
+		
+		m_lastUpdate = System.currentTimeMillis();
+		m_serial = System.currentTimeMillis();
+	}
+	
 	/**
 	 * Use IS and cascade IDs since this entry depends on a specific
 	 * cascade and the info service we're retrieving it from.
 	 */
-	public String getId() 
+	public String getId()
 	{
 		return m_strCascadeId + "." + m_strInfoServiceId;
 	}
@@ -57,8 +94,10 @@ public class PerformanceEntry extends AbstractDatabaseEntry
 			m_lDelay += (m_aDelays[i-1] = m_aDelays[i]);
 		m_aDelays[m_aDelays.length-1] = a_lDelay;
 		m_lDelay = (m_lDelay + a_lDelay) / m_aDelays.length;
+		
+		m_lastUpdate = System.currentTimeMillis();
 	}
-
+	
 	public void updateSpeed(double a_dSpeed) 
 	{
 		if(m_aSpeeds == null)
@@ -68,9 +107,11 @@ public class PerformanceEntry extends AbstractDatabaseEntry
 			m_dSpeed += (m_aSpeeds[i-1] = m_aSpeeds[i]);
 		m_aSpeeds[m_aSpeeds.length-1] = a_dSpeed;
 		m_dSpeed = (m_dSpeed + a_dSpeed) / m_aSpeeds.length;
+		
+		m_lastUpdate = System.currentTimeMillis();
 	}
 
-	public double getAverageThroughput()
+	public double getAverageSpeed()
 	{
 		return m_dSpeed;
 	}
@@ -78,5 +119,16 @@ public class PerformanceEntry extends AbstractDatabaseEntry
 	public long getAverageDelay()
 	{
 		return m_lDelay;
+	}
+	
+	public Element toXmlElement(Document a_doc)
+	{
+		Element elem = a_doc.createElement(XML_ELEMENT_NAME);
+		XMLUtil.setAttribute(elem, XML_ATTR_ID, getId());
+		XMLUtil.setAttribute(elem, XML_ATTR_AVG_DELAY, m_lDelay);
+		XMLUtil.setAttribute(elem, XML_ATTR_AVG_SPEED, m_dSpeed);
+		XMLUtil.setAttribute(elem, XML_ATTR_EXPIRE_TIME, getExpireTime());
+		
+		return elem;
 	}
 }
