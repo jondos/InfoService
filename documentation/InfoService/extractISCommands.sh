3f9d0e46f9fcab4762e3f7e0a3ca1a9f4a8e4268
sk13 <sk13@7f34aca7-edc8-46a5-9550-c19c22dae522>
2009-09-08 07:00:43 +0000

    Added some more descriptions for InfoService commands. There are still some missing -- if you now what they do, please describe them!

--- documentation/InfoService/extractISCommands.sh
+++ documentation/InfoService/extractISCommands.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-InfoServiceCommands_java="../../src/main/java/infoservice/InfoServiceCommands.java"
-sed -e "N;N;N;N;s/^.*Full Command: *\(\(GET\)\|\(POST\)\) *\/\([^ ^\n^\/]*\)\(\/\[\([^\n]*\)\]\)*.*Source: *\([^\n]*\).*Category: *\([^\n]*\).*Description_de: *\([^\n]*\)/\\\\isCommand{\8}{\1}{\4}{\6}{\7}{\9}/i;P;D;D;D;D;D" ${InfoServiceCommands_java} |grep isCommand
-#  sed -e "N;N;N;N;s/^.*Full Command: *\(\(GET\)\|\(POST\)\) *\/\([^ ^\n^\/]*\)\(\/\[\([^\n]*\)\]\)*.*Source: *\([^\n]*\).*Category: *\([^\n]*\).*/\\\\isCommand{\1}/i;P;D;D;D;D;D" ${InfoServiceCommands_java} |grep isCommand
+#Note: If this script does not run on cygwin check if the line ending is UNIX
+InfoServiceCommands_java=../../src/main/java/infoservice/InfoServiceCommands.java
+sed -e "N;N;N;N;s/^.*Full Command: *\(\(GET\)\|\(POST\)\) *\/\([^ ^\n^\/]*\)\(\/\[\([^\n]*\)\]\)*.*Source: *\([^\n]*\).*Category: *\([^\n]*\).*Description_de: *\([^\n]*\)/\\\\isCommand{\8}{\1}{\4}{\6}{\7}{\9}/i;P;D;D;D;D;D" ${InfoServiceCommands_java} | grep "isCommand"
