d2c5aa5467d764fc22080b2f1a7a3ba94a5a01d2
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2010-09-03 12:03:42 +0000

    enhanced key generation process

--- InfoService/trunk/src/main/java/infoservice/KeyGenTest.java
+++ InfoService/trunk/src/main/java/infoservice/KeyGenTest.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.SecureRandom;
 import java.util.GregorianCalendar;
+import java.util.Hashtable;
 
 import anon.crypto.DSAKeyPair;
+import anon.crypto.PKCS10CertificationRequest;
 import anon.crypto.PKCS12;
 import anon.crypto.Validity;
 import anon.crypto.X509DistinguishedName;
@@ -21,22 +23,56 @@ public class KeyGenTest
 	{
 		PKCS12 ownCertificate;
 		String strInfoServiceName;
+		String strCountry = "";
+		String strOrganization = "";
+		String strEMail = "";
+		
 		if (isName != null && isName.length() > 0)
 		{
 			strInfoServiceName = isName;
 		}
 		else
 		{
-			System.out.print("Please enter a name for the InfoService: ");
-			BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
-			strInfoServiceName = din.readLine();
+			strInfoServiceName = "";
+			while (strInfoServiceName.trim().length() == 0)
+			{
+				System.out.println("Please enter a name for the InfoService: ");
+				BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
+				strInfoServiceName = din.readLine();
+			}
 		}
+
+		while (strCountry.trim().length() != 2)
+		{
+			System.out.println("Please enter the ISO 2 country code of the country where your InfoService is located: ");
+			BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
+			strCountry = din.readLine();
+		}
+		
+		while (strOrganization.trim().length() == 0)
+		{
+			System.out.println("Please enter the name of your organization: ");
+			BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
+			strOrganization = din.readLine();
+		}
+		
+		while (strEMail.trim().length() == 0)
+		{
+			System.out.println("Please enter the e-mail address of your organization: ");
+			BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
+			strEMail = din.readLine();
+		}
+		
+		
 		String strPasswd = "";
 		if (passwd == null)
 		{
-			System.out.print("Please enter a password to protect the private key of the InfoService: ");
+			
+			/*
+			System.out.println("Please enter a password to protect the private key of the InfoService: ");
 			BufferedReader din = new BufferedReader(new InputStreamReader(System.in));
 			strPasswd = din.readLine();
+			*/
 		}
 		else
 		{
@@ -46,13 +82,28 @@ public class KeyGenTest
 		DSAKeyPair keyPair = DSAKeyPair.getInstance(new SecureRandom(), 1024, 80);
 		FileOutputStream out1 = new FileOutputStream("private.pfx");
 		FileOutputStream out2 = new FileOutputStream("public.cer");
+		FileOutputStream out3 = new FileOutputStream("public.csr");
 		X509Extensions extensions = new X509Extensions(new X509SubjectKeyIdentifier(keyPair.getPublic()));
-		ownCertificate = new PKCS12(new X509DistinguishedName("cn=" + strInfoServiceName), keyPair,
+		Hashtable<String, String> hashtable = new Hashtable<String, String>();
+		hashtable.put(X509DistinguishedName.IDENTIFIER_CN, strInfoServiceName);
+		hashtable.put(X509DistinguishedName.IDENTIFIER_C, strCountry);
+		hashtable.put(X509DistinguishedName.IDENTIFIER_E, strEMail);
+		hashtable.put(X509DistinguishedName.IDENTIFIER_O, strOrganization);
+		
+		
+		X509DistinguishedName dn = new X509DistinguishedName(hashtable);
+		ownCertificate = new PKCS12(dn, keyPair,
 									new Validity(new GregorianCalendar(), 5), extensions);
+		PKCS10CertificationRequest csrInfo = new PKCS10CertificationRequest(dn, keyPair, extensions);
+		
+		
 		ownCertificate.store(out1, strPasswd.toCharArray());
 		ownCertificate.getX509Certificate().store(out2);
+		csrInfo.toOutputStream(out3, true);
+		
 		out1.close();
 		out2.close();
+		out3.close();
 		System.out.println("Key generation finished!");
 	}
 }
