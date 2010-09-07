15cd2bdb32bd0aa92a13b284f75e121310f6ab03
rolf <rolf@7f34aca7-edc8-46a5-9550-c19c22dae522>
2010-09-07 14:48:24 +0000

    new version number

--- src/main/java/infoservice/InfoService.java
+++ src/main/java/infoservice/InfoService.java
@@ -61,7 +61,7 @@ public class InfoService implements Observer
 	/**
 	 * This is the version number of the infoservice software.
 	 */
-	public static final String INFOSERVICE_VERSION = "IS.08.077"; //never change the layout of this line!
+	public static final String INFOSERVICE_VERSION = "IS.08.078"; //never change the layout of this line!
 	
 	protected JWSInternalCommands oicHandler;
 
